4de2363dfba3888f3a7d3b4d080c3ec2a63b7c61
Peter Jones <peter.jones@loyaltylogistix.com>
2021-10-14 10:20:28 +0100

    Added ClientSettings for identity

---
+++ Common/src/SecuredAPI.SharedKernel/Contracts/IClientSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SecuredAPI.SharedKernel.Contracts
+{
+    public interface IClientSettings
+    {
+        string Name { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
---
+++ Identity/src/SecuredAPI.Identity/Configuration/ClientSettings.cs
@@ -0,0 +1,15 @@
+﻿using SecuredAPI.SharedKernel.Contracts;
+
+namespace SecuredAPI.Identity.Configuration
+{
+    public class ClientSettings : IClientSettings
+    {
+        public const string CONFIG_NAME = "ClientSettings";
+
+        public static ClientSettings Instance { get; } = new ClientSettings();
+        private ClientSettings() { }
+
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
